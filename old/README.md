86f7a3f1e377551b4f9181cd3cc72344cf58e224
lrossi93 <lo.reds1993@gmail.com>
2025-07-13 17:23:20 +0200

    optimizations

--- old/README.md
+++
@@ -1,2 +0,0 @@
-# My website
-A very basic portfolio. The most interesting stuff is [WinBox.js](https://nextapps-de.github.io/winbox/) (also, check the [repo](https://github.com/nextapps-de/winbox)).
--- old/js/loader.js
+++
@@ -1,6 +0,0 @@
-//display full-screen window loader gif while page loads
-$(window).load(function() {
-	//fade out when page completes loading
-	$(".loading-spinner").fadeOut("slow");
-	//$("#lrossi").fadeIn("slow");
-});
